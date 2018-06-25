06cf3bdf7101f5ff8e95fb74a9ff855a4af228a0
wxc <wangxinchun456@163.com>
2018-06-25 19:00:55 +0800

    登录功能完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -2,16 +2,22 @@
  * Created by dell on 2018/6/25.
  */
 
-/*
- * 1. 进行表单校验配置
- *    校验要求:
- *        (1) 用户名不能为空, 长度为2-6位
- *        (2) 密码不能为空, 长度为6-12位
- * */
-
 $(function () {
 
-  $('.form-horizontal').bootstrapValidator({
+  /*
+   * 1. 进行表单校验配置
+   *    校验要求:
+   *        (1) 用户名不能为空, 长度为2-6位
+   *        (2) 密码不能为空, 长度为6-12位
+   * */
+  $('#form').bootstrapValidator({
+    // 指定校验时的图标显示，默认是bootstrap风格
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+
     // 指定校验字段
     fields : {
       // 1.校验用户名
@@ -26,6 +32,9 @@ $(function () {
             min : 2,
             max : 6,
             message : '用户名长度为 2-6 位'
+          },
+          callback : {
+            message : '用户名不存在!'
           }
         }
       },
@@ -41,10 +50,44 @@ $(function () {
             min : 6,
             max : 12,
             message : '密码长度为 6-12 位'
+          },
+          callback : {
+            message : '密码错误!'
           }
         }
       }
     }
-  })
+  });
 
+
+  // 2.注册表单提交成功事件
+  $('#form').on('success.form.bv', function (e) {
+    e.preventDefault();
+    $.ajax({
+      type : 'post',
+      url : '/employee/employeeLogin',
+      data : $('#form').serialize(),
+      dataType : 'json',
+      success : function (info) {
+        // 2.1 验证通过跳转主页
+        if (info.success) {
+          location.href = 'index.html';
+        }
+        // 2.2 错误1000,用户名不存在
+        if (info.error === 1000) {
+          $('#form').data('bootstrapValidator').updateStatus('username', 'INVALID', 'callback');
+        }
+        // 2.3 错误1001,密码错误
+        if (info.error === 1001) {
+          $('#form').data('bootstrapValidator').updateStatus('password', 'INVALID', 'callback');
+        }
+      }
+    });
+  });
+
+
+  // 3.点击重置按钮,重置内容和样式
+  $('[type="reset"]').on('click', function () {
+    $('#form').data('bootstrapValidator').resetForm();
+  })
 });
